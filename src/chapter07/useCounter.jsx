3daaea3a6f3217d2ff0553d7a16f0c4d53c8165d
euncheol-kim <klmeuncheol@kakao.com>
2025-01-04 18:50:18 +0900

    modified: section08/chapter07

--- src/chapter07/useCounter.jsx
+++ src/chapter07/useCounter.jsx
@@ -3,10 +3,10 @@ import React, {useState} from "react";
 function useCounter(initialValue) {
     const [count, setCount] = useState(initialValue);
 
-    const increaseCount = () => setCount((count) => count + 1);
-    const decreaseCount = () => setCount((count) => Math.max(count - 1, 0));
+    const handleIncreaseCount = () => setCount((count) => count + 1);
+    const handleDecreaseCount = () => setCount((count) => Math.max(count - 1, 0));
 
-    return [count, increaseCount, decreaseCount];
+    return [count, handleIncreaseCount, handleDecreaseCount];
 }
 
 export default useCounter;
